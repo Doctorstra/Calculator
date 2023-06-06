fe6b0e4687c4dbab7e86b69d00efaede85fb655f
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2023-06-06 15:21:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Doctorstra/Calculator /Eva
+  git clone https://github.com/Doctorstra/Calculator /Calculator
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /Calculator
 fi
-cd /Eva
+cd /Calculator
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
